6f26c24dadfc1588fad6d697b257e18cac56926a
Evgeniy <selsel1994@inbox.ru>
2022-12-03 00:47:44 +0300

    ДЗ к 1 семинару

---
+++ task2/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+Console.Write("Введите число №1: ");
+int number1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число №2: ");
+int number2 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число №3: ");
+int number3 = int.Parse(Console.ReadLine()!);
+int [] numbers = { number1,number2,number3 };
+int size=3;
+int index=1;
+int max=numbers[0];
+while (index<size)
+{
+    if (max < numbers [index])
+    {
+        max = numbers [index];
+    }
+	else
+    {
+        index++;
+    }
+}
+Console.Write($"Максимальное число = {max} ");
