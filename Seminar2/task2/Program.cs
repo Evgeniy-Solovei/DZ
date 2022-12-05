6fdf0550a98e12ac8d377f86d127a4b0d095f2ce
Evgeniy <selsel1994@inbox.ru>
2022-12-05 23:30:09 +0300

    Добавил ДЗ. 2 семинар

--- Seminar2/task2/Program.cs
+++ Seminar2/task2/Program.cs
@@ -3,9 +3,9 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
-// Задумка была перевернуть число делением на 10 с остатком и добавлять остаток в максимальный индекс массива,
-// затем уменьшать максимаьный индекс на 1 и добалять следующий остаток в массив. Затем вызвать индекс 2 (решив
-// условие задачи,но у меня не вышло )
+// Задумка была перевернуть число делением на 10 с остатком и добавлять остаток в индекс 0 массива,
+// затем увеличивать  индекс0 на 1 и добавлять следующий остаток в массив. Затем вызвать индекс 2 массива (решив
+// условие задачи,но у меня не вышло.)
 // Console.WriteLine("Введите число");
 // int number1 = int.Parse(Console.ReadLine()!);
 // int[]=new Array();
@@ -13,7 +13,7 @@
 // if (number1 > -99 || number1 > 99)
 // {
 //     max_index = number1 % 10;
-//     max_index--;
+//     max_index++;
 //     Console.WriteLine("{numbers} [index 2]");
 // }
 // else
@@ -21,20 +21,43 @@
 //     Console.WriteLine($"В числе {number1} нет третьей цифры");
 // }
 
+// Console.WriteLine("Введите число");
+// int number1 = int.Parse(Console.ReadLine()!);
+// int number2 = 0;
+// int number3 = 0; // это будет перевернутое число number1
+// int number4 = 0; // 3 цифра number1
+// while (number1 > 0) 
+// {
+//     number2 = number1 % 10; // по какой формуле перевернуть число искал в гугле, сам не додумался. Я думал что надо %, добавлять в переменную, затем число без прошлого остатка опять % и добавлять в перемееную.Как в переменную просто складывать число одно за другим без каких либо математических действий я не нашёл.
+//     number1 = number1 / 10;
+//     number3 = number3 * 10 + number2;
+// }
+// {
+//     number3 = number3 / 100;
+//     number4 = number3 % 10;
+
+// }
+// if (number1 < -100 || number1 < 99)
+// {
+//     Console.WriteLine(number4);
+// }
+// else
+// {
+//     Console.WriteLine($"В числе {number1} нет третьей цифры");
+// }
+
 Console.WriteLine("Введите число");
-int number1 = int.Parse(Console.ReadLine()!);
-int number2; // это будет перевернутое число number1
-int number3; // переменная показывающая 3 цифру
-if (number1 < -99 || number1 > 99)
+int number1 = int.Parse(Console.ReadLine()!); // можно было использовать переменную long из за возможности ввести больше числа
+while (number1 > 1000)
 {
-    number2 = number1 % 10;
-        number3=number2%10;
-        number3=number2%10;
-        number3=number2%10;
-        Console.WriteLine(number3);
+    number1 = number1 / 10;
+}
+if (number1 < 1000)
+{
+    number1 = number1 % 10;
+    Console.WriteLine(number1);
 }
 else
 {
     Console.WriteLine($"В числе {number1} нет третьей цифры");
 }
-
--- Seminar2/task3/Program.cs
+++ Seminar2/task3/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, 
+// является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите цифру от 1 до 7, обозначающую день недели: ");
+int number1 = int.Parse(Console.ReadLine()!);
+if (number1 < 6 && number1 > 0)
+{
+    Console.WriteLine("No");
+}
+else if (number1 > 5 && number1 < 8)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("Ошибка, вы ввели число меньше 1 или больше 7");
+}
