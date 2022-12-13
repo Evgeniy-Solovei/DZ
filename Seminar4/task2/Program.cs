95dba7cbc3e3dd745318855d97069542a2097173
Evgeniy <selsel1994@inbox.ru>
2022-12-13 19:39:10 +0300

    DZ 4 seminar

---
+++ Seminar4/task2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число: ");
+int num1=int.Parse(Console.ReadLine()!);
+int sum = num1%10;
+while (num1>0)
+{
+    num1=num1/10;
+    sum=num1%10+sum;
+}
+// return sum;
+Console.Write($"Cумма цифр числа {num1} = {sum}");
---
+++ Seminar4/task3/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] array = new int[8];
+int[] InputArr(int[] number)
+{
+    for (int i = 0; i < number.Length; i++)
+    {
+        Console.Write($"Введите элемент массива под индексом {i}: "); 
+        number[i] = int.Parse(Console.ReadLine()!);
+    }
+    return number;
+}
+int[] PrintArray(int[] number)
+{
+    Console.Write("Array - [");
+    for (int i = 0; i < number.Length; i++)
+    {
+        Console.Write($"{number[i]}," );
+    }
+    Console.Write("\b]");
+    return number;
+}
+
+InputArr(array);
+PrintArray(array);
+
+
