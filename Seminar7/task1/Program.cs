50db3489a8850899f04323ec53bf1b36af34b81c
Евгений Соловей <solovey.e.v.@MacBook-Air-Evgenij.local>
2022-12-26 01:20:17 +0300

    DZ 7 Seminar

---
+++ Seminar7/task1/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+double[,] GetArray(int m, int n) //Создаём метод с названием GetArray с аргументами m - строки. n - столбцы. 
+{
+    double[,] result = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+               result[i, j] = Math.Round(new Random().NextDouble()+new Random().Next(-10, 10),2); // math.round(number,2)где number - вещественное число у которого мы ходим оставить (2), знака после запятой 
+        }
+    }
+    return result;
+}
+
+double [,] PrintArray(double [,] inArray) 
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write(inArray[i, j] + "  ");
+        }
+        Console.WriteLine();
+        Console.WriteLine();
+    }
+    return inArray;
+}
+
+
+Console.WriteLine("Введите колличество строк массива m:");
+int rows = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите колличество столбцов массива n:");
+int columns = int.Parse(Console.ReadLine()!);
+double [,] array = GetArray(rows,columns);
+PrintArray(array);
---
+++ Seminar7/task2/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:           1 4 7 2
+//                                   5 9 2 3
+//                                   8 4 2 4       17 -> такого числа в массиве нет
+int[,] GetArray(int m, int n) //Создаём метод с названием GetArray с аргументами m - строки. n - столбцы. 
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(-20, 20);
+        }
+    }
+    return result;
+}
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write(inArray[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+void numberArray(int [,] array, int rows, int columns)
+{
+    int m = 4;
+    int n = 5;
+    if (rows > m || columns > n)
+    {
+        Console.WriteLine("Такого числа в массиве нет. ");
+    }
+    else
+    {
+        Console.WriteLine($"{array [rows,columns]}");
+    }
+}
+int[,] array = GetArray(4, 5);
+PrintArray(array);
+Console.WriteLine("Введите строку искомого числа в массива m:");
+int rows = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите столбец искомого числа массива n:");
+int columns = int.Parse(Console.ReadLine()!);
+Console.WriteLine();
+numberArray(array, rows, columns);
