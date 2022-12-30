439a59cdab186cafd11d17bb0b05d12357852ba2
Евгений Соловей <solovey.e.v.@MacBook-Air-Evgenij.local>
2022-12-30 17:57:36 +0300

    DZ 8 Seminar

---
+++ Seminar8/task1/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2.     1 2 3 4 5
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+int[,] GetArray(int m, int n) //Создаём метод с названием GetArray с аргументами m - строки. n - столбцы. 
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(1, 11);
+        }
+    }
+    return result;
+}
+void PrintArray(int[,] inArray)   // Создали метод для вывода массива в консоль
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write(inArray[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] maxToMin(int[,] maxMin)
+{
+
+    for (int i = 0; i < maxMin.GetLength(0); i++)
+    {
+        for (int j = 0; j < maxMin.GetLength(1); j++)
+        {
+            for (int k = 0; k < maxMin.GetLength(1) - 1; k++)
+            {
+                if (maxMin[i, k] < maxMin[i, k + 1])
+                {
+                    int max = maxMin[i, k + 1];
+                    maxMin[i, k + 1] = maxMin[i, k];
+                    maxMin[i, k] = max;
+                }
+            }
+        }
+    }
+    return maxMin;
+}
+
+int[,] array = GetArray(4, 3);
+PrintArray(array);
+maxToMin(array);
+Console.WriteLine();
+PrintArray(array);
---
+++ Seminar8/task2/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int[,] GetArray(int m, int n) //Создаём метод с названием GetArray с аргументами m - строки. n - столбцы. 
+{
+    int[,] result = new int[m, n];
+    if (m == n)
+    {
+        Console.WriteLine("Не верные знвчения, для прямоугольного массива");
+    }
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(1, 11);
+        }
+    }
+    return result;
+}
+void PrintArray(int[,] inArray)   // Создали метод для вывода массива в консоль
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write(inArray[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] minString(int[,] array)
+{
+    int minRow = 0;
+    int minSumRow = 0;
+    int sumRow = 0;
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        minRow = minRow + array[0, i];
+    }
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++) 
+        {
+            sumRow = sumRow + array[i, j];
+        }
+        if (sumRow < minRow)
+        {
+            minRow = sumRow;
+            minSumRow = i;
+        }
+        sumRow = 0;
+    }
+    Console.Write($"{minSumRow + 1} строка с наименьшей суммой элементов");
+    return array;
+}
+
+int[,] array = GetArray(5, 5);
+PrintArray(array);
+Console.WriteLine();
+minString(array);
+
